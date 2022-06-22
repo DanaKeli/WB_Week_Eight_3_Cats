17327c0a14051652ab87808e4141ef1b4268ff3d
DanaKeli <lunata_@mail.ru>
2022-06-22 15:06:50 +0300

    app renamed

--- app/src/main/java/com/example/catsapp/ui/FavoriteFragment.kt
+++ app/src/main/java/com/example/catsapp/ui/FavoriteFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.catsapp.databinding.FragmentFavoriteBinding
@@ -17,7 +16,6 @@ class FavoriteFragment : Fragment() {
 
     private var _binding: FragmentFavoriteBinding? = null
     private val binding get() = _binding!!
-    private lateinit var fm: FragmentManager
     private lateinit var adapter: FavoriteAdapter
     private lateinit var vm: CatVM
 
@@ -27,7 +25,6 @@ class FavoriteFragment : Fragment() {
     ): View? {
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         vm = ViewModelProvider(requireActivity())[CatVM::class.java]
-        fm = requireActivity().supportFragmentManager
         initView()
         return binding.root
     }
